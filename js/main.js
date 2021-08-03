963eb45b259f919eb91204f6a609198b8e4eaed1
FairoozAzim <37416649+FairoozAzim@users.noreply.github.com>
2021-08-03 18:09:59 +0600

    carousel and posts done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//read more
+
+$readMoreJS.init({
+    target: '.read-more p', // Selector of the element the plugin applies to (any CSS selector, eg: '#', '.'). Default: ''
+    numOfWords: 2, // Number of words to initially display (any number). Default: 50
+    toggle: true, // If true, user can toggle between 'read more' and 'read less'. Default: true
+    moreLink: 'read more ...', // The text of 'Read more' link. Default: 'read more ...'
+    // The text of 'Read less' link. Default: 'read less'
+});
